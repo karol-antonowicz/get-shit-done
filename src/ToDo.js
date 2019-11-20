8bcd1154036e827f54aa73d1a143acb7d8635fde
Karol Antonowicz <karol.antonowicz@gmail.com>
2019-11-20 20:20:58 +0100

    initial commit: created firebase database, added input box in app where you put tasks, add button sends the task json format to firebase database using post method

---
+++ src/ToDo.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+
+class ToDo extends React.Component{
+    constructor(props){
+        super(props)
+        this.state={
+            text:""
+        }
+    }
+
+    addToDo = event => {
+        event.preventDefault();
+
+        fetch('https://to-do-app-5f68e.firebaseio.com/todo.json', {
+            method: 'POST',
+            body: JSON.stringify({text:this.state.text, active:"true"})
+        });
+    }
+
+    render(){
+        return(
+            <form onSubmit={this.addToDo}>
+                <input placeholder="put your shit here" value={this.state.value} onChange={e=>this.setState({text:e.target.value})}></input>
+                <button type="submit" >Add Task</button>
+            </form>
+        )
+    }
+}
+
+export default ToDo
