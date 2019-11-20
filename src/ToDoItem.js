8fda9f82a2543223824c62c76e9f81ec5e4a202b
Karol Antonowicz <karol.antonowicz@gmail.com>
2019-11-20 22:20:33 +0100

    added delete button if u wanna delete the task

--- src/ToDoItem.js
+++ src/ToDoItem.js
@@ -5,7 +5,8 @@ class ToDoItem extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            active: props.todo.active
+            active: props.todo.active,
+            todo: props.todo
 
         }
     }
@@ -16,9 +17,14 @@ class ToDoItem extends React.Component {
             method: 'PATCH',
             body: JSON.stringify({active:newActive})
         });
-        console.log(this.state.active)
-        console.log(this.props.todo)
         this.setState({active:newActive})
+
+    }
+
+    delete(){
+        fetch(`https://to-do-app-5f68e.firebaseio.com/todo/${this.props.todo.id}.json`, {
+            method: 'DELETE',
+        });
 
     }
 
@@ -26,8 +32,10 @@ class ToDoItem extends React.Component {
         return(
         <div className="item">
             <div className="text">{this.props.todo.text}</div>
-            <div className="active"><button onClick={()=>this.toggleActive()}>activate</button>{this.state.active?'ACTIVE':'NOT ACTIVE'}</div>
+            <div className="active">{this.state.active?'ACTIVE':'NOT ACTIVE'}</div>
             <div className="id">{this.props.todo.id}</div>
+            <button onClick={()=>this.toggleActive()}>activate</button>
+            <button onClick={()=>this.delete()}>delete</button>
         </div>
         )
     }
