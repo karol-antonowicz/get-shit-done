73cbb66dbb019b68ce43a0950b61994dc16aae0d
Karol Antonowicz <karol.antonowicz@gmail.com>
2019-11-25 20:33:52 +0100

    checkbox check if task done

--- src/ToDoItem.js
+++ src/ToDoItem.js
@@ -8,60 +8,59 @@ class ToDoItem extends React.Component {
         this.state = {
             active: props.todo.active,
             todo: props.todo,
-            todoText:props.todo.text
+            todoText: props.todo.text
 
 
         }
     }
 
-    toggleActive(){
+    toggleActive() {
         const newActive = !this.state.active
         fetch(`https://to-do-app-5f68e.firebaseio.com/todo/${this.props.todo.id}.json`, {
             method: 'PATCH',
-            body: JSON.stringify({active:newActive})
+            body: JSON.stringify({ active: newActive })
         });
-        this.setState({active:newActive})
+        this.setState({ active: newActive })
 
     }
 
-    delete(){
+    delete() {
         fetch(`https://to-do-app-5f68e.firebaseio.com/todo/${this.props.todo.id}.json`, {
             method: 'DELETE',
         });
 
     }
 
-    handleTextChange = newValue => {   
+    handleTextChange = newValue => {
         console.log(this.state.todoText)
         this.setState({
-          todoText: newValue
+            todoText: newValue
         });
-      };
+    };
 
-      updateTodo(){
+    updateTodo() {
         const newText = this.state.todoText
         fetch(`https://to-do-app-5f68e.firebaseio.com/todo/${this.props.todo.id}.json`, {
             method: 'PATCH',
-            body: JSON.stringify({text:newText})
+            body: JSON.stringify({ text: newText })
         });
-        this.setState({todoText:newText})
+        this.setState({ todoText: newText })
 
 
     }
 
-      
-    
 
-    render(){
-        return(
-        <div className="item">
-            <div className='text'><input onKeyPress={event=>{if(event.key==='Enter')  {  this.updateTodo() ;}}}  onChange={event => {
-        this.handleTextChange(event.target.value);
-      }} className='inputText' type='text' value={this.state.todoText}></input></div>
-            <div className="active">{this.state.active?'ACTIVE':'NOT ACTIVE'}</div>
-            <button onClick={()=>this.toggleActive()}>{this.state.active?'Deactivate':'Activate'}</button>
-            <a className="delete" onClick={()=>this.delete()}><img className="cross" src={Delete}></img></a>
-        </div>
+    // {this.state.active?'ACTIVE':'NOT ACTIVE'}
+
+    render() {
+        return (
+            <div className="item">
+                <div className="toggle"><input onClick={() => this.toggleActive()} type="checkbox" className="tickToggle"></input></div>
+                <div className='text'><input onKeyPress={event => { if (event.key === 'Enter') { this.updateTodo(); } }} onChange={event => {
+                    this.handleTextChange(event.target.value);
+                }} className='inputText' type='text' value={this.state.todoText}></input></div>
+                <a className="delete" onClick={() => this.delete()}><img className="cross" src={Delete}></img></a>
+            </div>
         )
     }
 }
